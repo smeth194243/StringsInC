c51b67453263270e1dcf518b8afc0b9fd9637b9f
smeth194243 <smor7432@740s-pl-106.local>
2017-02-02 13:10:21 -0700

    Initial Commit

---
+++ StringsInC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StringsInC
+//
+//  Created by Morris, Seth on 2/2/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
